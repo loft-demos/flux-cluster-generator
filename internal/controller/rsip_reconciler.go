2d152272a3582b048c31556f33123a9ac063f279
Kurt Madel <kurt.madel@loft.sh>
2025-09-08 09:48:31 -0400

    Update rsip_reconciler.go

--- internal/controller/rsip_reconciler.go
+++ internal/controller/rsip_reconciler.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/sets"
